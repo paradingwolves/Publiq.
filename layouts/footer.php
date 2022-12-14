cb2a444ccfe90e536513ab267f8b98cb3a204732
Justin Brierley <91489058+paradingwolves@users.noreply.github.com>
2022-12-14 15:47:58 -0500

    Refactored project files, added some content

---
+++ layouts/footer.php
@@ -0,0 +1,27 @@
+</main>
+        <div class="row">   
+            <div class="container-fluid">
+                <footer class="col-md-12  bg-dark">
+                    <div class="row">
+                        <div class="col-lg-4"></div>
+                        <div class="col-lg-1"><a href="index.php">Home</a></div>
+                        <div class="col-lg-1"><a href="cocktails.php">Cocktails</a></div>
+                        <div class="col-lg-1"><a href="hours.php">Hours</a></div>
+                        <div class="col-lg-1"><a href="About.php">Fundraising</a></div>
+                        <div class="col-lg-4"></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-4"></div>
+                        <div class="col-lg-4"><h4>Publiq</h4></div>
+                        <div class="col-lg-4"></div>
+                    </div>
+                </footer>
+            </div>
+        </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+        <script type="text/javascript" src="../js/script.js"></script>
+        <script type="text/javascript" src="../js/validation.js"></script>
+    </body>
+</html>
